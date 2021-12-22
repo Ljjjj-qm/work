196170c1d6b333f9d07a2523799e47984b20543e
ZQL666-No1 <977139874@qq.com>
2021-12-22 11:20:59 +0800

    学生管理功能

---
+++ TORTUR/src/main/java/com/tutor/service/AppriseService.java
@@ -0,0 +1,12 @@
+package com.tutor.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.tutor.entity.Apprise;
+
+/**
+ * @Author : ZQL
+ * Date:2021/12/21
+ * Time:21:29
+ */
+public interface AppriseService extends IService<Apprise> {
+}
