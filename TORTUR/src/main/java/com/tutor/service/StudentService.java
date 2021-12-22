4844dc54703a24b9dc60ee7921c659878785ce36
成龙生 <1536463948@qq.com>
2021-12-22 11:49:44 +0800

    合并

--- TORTUR/src/main/java/com/tutor/service/StudentService.java
+++ TORTUR/src/main/java/com/tutor/service/StudentService.java
@@ -3,10 +3,13 @@ package com.tutor.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.tutor.entity.Student;
 
+import java.util.List;
+
 /**
  * @Author : ZQL
  * Date:2021/12/21
  * Time:20:53
  */
 public interface StudentService extends IService<Student> {
+    List<Student> getStudentList(Integer padeNum, int pageSize);
 }
--- TORTUR/src/main/java/com/tutor/service/impl/StudentServiceImpl.java
+++ TORTUR/src/main/java/com/tutor/service/impl/StudentServiceImpl.java
@@ -1,11 +1,14 @@
 package com.tutor.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.tutor.entity.Student;
 import com.tutor.mapper.StudentMapper;
 import com.tutor.service.StudentService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @Author : ZQL
@@ -38,4 +41,12 @@ public class StudentServiceImpl  extends ServiceImpl<StudentMapper, Student> imp
        return baseMapper.updateById(student);
     }
 
+    @Override
+    public List<Student> getStudentList(Integer padeNum, int pageSize) {
+        Page<Student> page = new Page<>(padeNum, pageSize);
+        QueryWrapper<Student> wrapper = new QueryWrapper<>();
+        wrapper.orderByAsc("id");
+        Page<Student> selectPage = baseMapper.selectPage(page, wrapper);
+        return selectPage.getRecords();
+    }
 }
